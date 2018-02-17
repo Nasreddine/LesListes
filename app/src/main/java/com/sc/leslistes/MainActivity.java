d86a15d84472941bb22e210649f45875df4b6c96
nasredine <nacer.cheniki@gmail.com>
2018-02-17 09:39:29 +0100

    add more comments to explain each part of the code

--- app/src/main/java/com/sc/leslistes/MainActivity.java
+++ app/src/main/java/com/sc/leslistes/MainActivity.java
@@ -24,27 +24,31 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        // faire le lien entre notre objet à programmer et l'interface XML qui concerne la liste
         liste = (ListView) findViewById(R.id.liste);
+        // créer la source de données pour la liste, donc notre cas simple il s'agit d'une liste de chaînes de caractères
+        ArrayList<String> source_donnees = new ArrayList<>();
+        source_donnees.add("élément1");
+        source_donnees.add("élément2");
 
-        ArrayList<String> elements = new ArrayList<>();
-        elements.add("élément1");
-        elements.add("élément2");
-
+        // créer un adaptateur qui joue le rôle d'un intermidiaire
+        // entre le coomposant graphique (liste) et la source des donnée (source_donnees)
+        // android.R.layout.simple_list_item_1 est une vue déjà définit par développeur Android
         ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(),
-                android.R.layout.simple_list_item_1,elements);
+                android.R.layout.simple_list_item_1, source_donnees);
 
+        // attacher l'adaptateur à notre liste
         liste.setAdapter(adapter);
 
-
-
+        // définir un événenement qui sera lancé quand on clique sur un élément de la liste
+        // on affiche un message contenat la position de l'élément cliqué et
         liste.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-          Log.i("MYTAG", "l'élément " + position +
-                        " a été cliqué");
-          Toast.makeText(getApplicationContext(),"l'élément " + position
-                        + " a été cliqué", Toast.LENGTH_LONG).show();
+                Log.i("MYTAG", "l'élément " + position + " a été cliqué");
+                String contenu = liste.getItemAtPosition(position).toString();
+                Toast.makeText(getApplicationContext(), "l'élément " + position + " a été cliqué"
+                                + " sont contenu :" + contenu, Toast.LENGTH_LONG).show();
             }
         });
 
